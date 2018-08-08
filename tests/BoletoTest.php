dd9012b1fa49914a39af495096d9dcb3d90c8d20
FrancoVigier <francovallejosvigier@gmail.com>
2018-08-08 13:26:09 -0300

    test semana 1

--- tests/BoletoTest.php
+++ tests/BoletoTest.php
@@ -7,9 +7,9 @@ use PHPUnit\Framework\TestCase;
 class BoletoTest extends TestCase {
 
     public function testSaldoCero() {
+		$val = [10,20,30,50,100,510.15,962.59];
         $valor = 14.80;
         $boleto = new Boleto($valor, NULL, NULL);
-
         $this->assertEquals($boleto->obtenerValor(), $valor);
     }
 }
