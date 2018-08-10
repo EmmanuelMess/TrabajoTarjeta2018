49970915412b1fe19f9eede71f0cbdc34f110c52
Emmanuel M <emmanuelbendavid@gmail.com>
2018-08-10 16:36:13 -0300

    Corrected increase for largest charge

--- src/Constantes.php
+++ src/Constantes.php
@@ -16,8 +16,8 @@ function valorCargado($monto) {
 
     if(!$VALORES_ADICIONADOS->contains($monto)) return $monto;
 
-    static $cargasMap = [51015 => (510.15+81.93), 962590 => (962.59+221.58)];
-    static $correccion = 10;
+    static $cargasMap = [51015 => (510.15+81.93), 96259 => (962.59+221.58)];
+    static $correccion = 100;
 
     return $cargasMap[$monto * $correccion];
 }
