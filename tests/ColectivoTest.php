0b053ec13d28b0000684b8fe8de39d329c396614
FrancoVigier <francovallejosvigier@gmail.com>
2018-08-12 23:09:28 -0300

    listo

--- tests/ColectivoTest.php
+++ tests/ColectivoTest.php
@@ -6,11 +6,13 @@ use PHPUnit\Framework\TestCase;
 
 class ColectivoTest extends TestCase {
 
-    public function testLinea() {
+    public function testLineaEmpresaNumero() {
 	$linea="143 Roja";
 	$numero=13;
 	$empresa="Rosario Bus";
 	$colectivo = new Colectivo($linea,$empresa,$numero);
 	$this->assertEquals($linea,$colectivo->linea());
+	$this->assertEquals($numero,$colectivo->numero());
+	$this->assertEquals($empresa,$colectivo->empresa());
     }
 }
