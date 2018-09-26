f146a7a28037e11fddc7fd42b681dbcf80ea3567
Emmanuel M <emmanuelbendavid@gmail.com>
2018-09-25 23:23:24 -0300

    Correjida indentacion

--- src/Tarjeta.php
+++ src/Tarjeta.php
@@ -6,173 +6,170 @@ class Tarjeta implements TarjetaInterface {
     protected $saldo = 0;
     protected $boletosPlusUsados = 0;
     protected $ultTiempo = -TiempoAyudante::CINCO_MINUTOS - 1;
-	protected $transbordo;
-	protected $linea='';
-	protected $bandera='';
+    protected $transbordo;
+    protected $linea = '';
+    protected $bandera = '';
 
-	public function __construct(){
-		$this->transbordo = 0;
-	}
-	
-	public function estransbordo($montotiempo, $lineacole, $banderacole){
-	global $DIASFERIADOS;
-	$diasferiados = $DIASFERIADOS;
-	if($lineacole!=$this->linea || $banderacole != $this->bandera){
-	
-		$this->linea = $lineacole;
-		$this->bandera = $banderacole;
+    public function __construct() {
+        $this->transbordo = 0;
+    }
 
-		if ( $this->transbordo == 0) {
-				$this->transbordo = $montotiempo;
-				echo date("\n((Y-m-d) (H:i:s))", $montotiempo);
-				return false;
-		}
-		
-		$tiempoayuda = new TiempoAyudante();
-		$tiempopago = $montotiempo;
-		$tolerancia = $this->transbordo;
-		
-		$horapago1 = $tiempoayuda->saber_hora($this->transbordo);
-		$horapago2 = $tiempoayuda->saber_hora($tiempopago);
+    public function estransbordo($montotiempo, $lineacole, $banderacole) {
+        global $DIASFERIADOS;
+        $diasferiados = $DIASFERIADOS;
+        if ($lineacole != $this->linea || $banderacole != $this->bandera) {
 
-		$diapago1 = $tiempoayuda->saber_dia($this->transbordo);//necesitaria un dia pago prima de formato 2018-09-19 por ejemplo
-		$diapagoferiado = $tiempoayuda->saber_dia_md($this->transbordo);
-		$diapago1prima = $tiempoayuda->saber_dia_ymd($this->transbordo);
+            $this->linea = $lineacole;
+            $this->bandera = $banderacole;
 
-		$diapago2prima = $tiempoayuda->saber_dia_ymd($tiempopago);
-		$diapago2 = $tiempoayuda->saber_dia($tiempopago);
+            if ($this->transbordo == 0) {
+                $this->transbordo = $montotiempo;
+                echo date("\n((Y-m-d) (H:i:s))", $montotiempo);
+                return false;
+            }
 
-		//$horapago2 = "23:00:00"; para controlar en el test
-		//	if ("06:00:00"<= "06:27:00"){echo "true";}
-		//	if ("22:00:00">= "06:27:00"){echo "true";}
-		echo $diapago1, " ", $diapago2;
-	
-		if( $diapago1 == "Lunes" || $diapago1 == "Martes" || $diapago1 == "Miercoles" || $diapago1 == "Jueves" || $diapago1 == "Viernes"){
-				if($horapago1 >= "06:00:00" && $horapago1 <= "22:00:00" && $diapago1 == $diapago2 && $diapago1prima == $diapago2prima){echo "DIA";
-					$tolerancia += (60 * 60);
-					$tolerancia = $tiempoayuda->saber_hora($tolerancia);
-					echo "TOLERANCIA " ,$tolerancia;
-					echo "HORA PAGO 2 ",$horapago2;
-						if($horapago2<=$tolerancia){
-							echo "DIA";
-				
-							$this->transbordo=0;
-							return true;
-						}
-						else{
-							echo "false DIA";
-							$this->transbordo=0;
-							return false;
-						}
-				}
-				if( $horapago1 > "22:00:00" || $horapago1 < "06:00:00"){echo "NOCHE";
-					$tolerancia += (90 * 60);
-					$tolerancia = $tiempoayuda->saber_hora($tolerancia);
-					echo "TOLERANCIA " ,$tolerancia;
-					echo "HORA PAGO 2 ",$horapago2;
-						if($horapago2<=$tolerancia){
-							echo "NOCHE";
-							$this->transbordo=0;
-							return true;
-						}
-						else{
-							echo "false NOCHE";
-							$this->transbordo=0;
-							return false;
-						}
-				}
-		}
+            $tiempoayuda = new TiempoAyudante();
+            $tiempopago = $montotiempo;
+            $tolerancia = $this->transbordo;
 
-		if( $diapago1 == "Sabado"){
-				if($horapago1 >= "06:00:00" && $horapago1 <= "14:00:00" && $diapago1 == $diapago2 && $diapago1prima == $diapago2prima){echo "DIA";
-					$tolerancia += 60 * 60;
-					$tolerancia = $tiempoayuda->saber_hora($tolerancia);
-					echo "TOLERANCIA " ,$tolerancia;
-					echo "HORA PAGO 2 ",$horapago2;
-						if($horapago2<=$tolerancia){
-							$this->transbordo=0;
-							return true;
-						}
-						else{
-							$this->transbordo=0;
-							return false;
-						}
-				}
-				if($horapago1 > "14:00:00" && $horapago1 <= "22:00:00" && $diapago1 == $diapago2 && $diapago1prima == $diapago2prima){echo "TARDE";
-					$tolerancia += 90 * 60;
-					$tolerancia = $tiempoayuda->saber_hora($tolerancia);
-					echo "TOLERANCIA " ,$tolerancia;
-					echo "HORA PAGO 2 ",$horapago2;
-						if($horapago2<=$tolerancia){
-							$this->transbordo=0;
-							return true;
-						}
-						else{
-							$this->transbordo=0;
-							return false;
-						}
-				}
+            $horapago1 = $tiempoayuda->saber_hora($this->transbordo);
+            $horapago2 = $tiempoayuda->saber_hora($tiempopago);
 
-				if( $horapago1 > "22:00:00" || $horapago1 < "06:00:00"){echo "NOCHE";
-					$tolerancia += 90 * 60;
-					$tolerancia = $tiempoayuda->saber_hora($tolerancia);
-					echo "TOLERANCIA " ,$tolerancia;
-					echo "HORA PAGO 2 ",$horapago2;
-						if($horapago2<=$tolerancia){
-							$this->transbordo=0;
-							return true;
-						}
-						else{
-							$this->transbordo=0;
-							return false;
-						}
-				}
+            $diapago1 = $tiempoayuda->saber_dia($this->transbordo);//necesitaria un dia pago prima de formato 2018-09-19 por ejemplo
+            $diapagoferiado = $tiempoayuda->saber_dia_md($this->transbordo);
+            $diapago1prima = $tiempoayuda->saber_dia_ymd($this->transbordo);
 
-		}
-		//$var = "01-01";
-		//if(in_array($var,$diasferiados)){echo "TTTTTTTT";}
+            $diapago2prima = $tiempoayuda->saber_dia_ymd($tiempopago);
+            $diapago2 = $tiempoayuda->saber_dia($tiempopago);
 
-		if( $diapago1 == "Domingo" || in_array($diapagoferiado,$diasferiados)){
-				if($horapago1 >= "06:00:00" && $horapago1 <= "22:00:00" && $diapago1 == $diapago2 && $diapago1prima == $diapago2prima){echo "TODO EL DIA";
-					$tolerancia += 90 * 60;
-					$tolerancia = $tiempoayuda->saber_hora($tolerancia);
-					echo "TOLERANCIA " ,$tolerancia;
-					echo "HORA PAGO 2 ",$horapago2;
-						if($horapago2<=$tolerancia){
-							$this->transbordo=0;
-							return true;
-						}
-						else{
-							$this->transbordo=0;
-							return false;
-						}
-				}
+            //$horapago2 = "23:00:00"; para controlar en el test
+            //	if ("06:00:00"<= "06:27:00"){echo "true";}
+            //	if ("22:00:00">= "06:27:00"){echo "true";}
+            echo $diapago1, " ", $diapago2;
 
-				if( $horapago1 > "22:00:00" || $horapago1 < "06:00:00"){
-					$tolerancia += 90 * 60;
-					$tolerancia = $tiempoayuda->saber_hora($tolerancia);
-						if($horapago2<=$tolerancia){
-							$this->transbordo=0;
-							return true;
-						}
-						else{
-							$this->transbordo=0;
-							return false;
-						}
-				}
-		}
-	}
-		else 
-		{
-				return false;
-		}
-	}
-	
+            if ($diapago1 == "Lunes" || $diapago1 == "Martes" || $diapago1 == "Miercoles" || $diapago1 == "Jueves" || $diapago1 == "Viernes") {
+                if ($horapago1 >= "06:00:00" && $horapago1 <= "22:00:00" && $diapago1 == $diapago2 && $diapago1prima == $diapago2prima) {
+                    echo "DIA";
+                    $tolerancia += (60 * 60);
+                    $tolerancia = $tiempoayuda->saber_hora($tolerancia);
+                    echo "TOLERANCIA ", $tolerancia;
+                    echo "HORA PAGO 2 ", $horapago2;
+                    if ($horapago2 <= $tolerancia) {
+                        echo "DIA";
+
+                        $this->transbordo = 0;
+                        return true;
+                    } else {
+                        echo "false DIA";
+                        $this->transbordo = 0;
+                        return false;
+                    }
+                }
+                if ($horapago1 > "22:00:00" || $horapago1 < "06:00:00") {
+                    echo "NOCHE";
+                    $tolerancia += (90 * 60);
+                    $tolerancia = $tiempoayuda->saber_hora($tolerancia);
+                    echo "TOLERANCIA ", $tolerancia;
+                    echo "HORA PAGO 2 ", $horapago2;
+                    if ($horapago2 <= $tolerancia) {
+                        echo "NOCHE";
+                        $this->transbordo = 0;
+                        return true;
+                    } else {
+                        echo "false NOCHE";
+                        $this->transbordo = 0;
+                        return false;
+                    }
+                }
+            }
+
+            if ($diapago1 == "Sabado") {
+                if ($horapago1 >= "06:00:00" && $horapago1 <= "14:00:00" && $diapago1 == $diapago2 && $diapago1prima == $diapago2prima) {
+                    echo "DIA";
+                    $tolerancia += 60 * 60;
+                    $tolerancia = $tiempoayuda->saber_hora($tolerancia);
+                    echo "TOLERANCIA ", $tolerancia;
+                    echo "HORA PAGO 2 ", $horapago2;
+                    if ($horapago2 <= $tolerancia) {
+                        $this->transbordo = 0;
+                        return true;
+                    } else {
+                        $this->transbordo = 0;
+                        return false;
+                    }
+                }
+                if ($horapago1 > "14:00:00" && $horapago1 <= "22:00:00" && $diapago1 == $diapago2 && $diapago1prima == $diapago2prima) {
+                    echo "TARDE";
+                    $tolerancia += 90 * 60;
+                    $tolerancia = $tiempoayuda->saber_hora($tolerancia);
+                    echo "TOLERANCIA ", $tolerancia;
+                    echo "HORA PAGO 2 ", $horapago2;
+                    if ($horapago2 <= $tolerancia) {
+                        $this->transbordo = 0;
+                        return true;
+                    } else {
+                        $this->transbordo = 0;
+                        return false;
+                    }
+                }
+
+                if ($horapago1 > "22:00:00" || $horapago1 < "06:00:00") {
+                    echo "NOCHE";
+                    $tolerancia += 90 * 60;
+                    $tolerancia = $tiempoayuda->saber_hora($tolerancia);
+                    echo "TOLERANCIA ", $tolerancia;
+                    echo "HORA PAGO 2 ", $horapago2;
+                    if ($horapago2 <= $tolerancia) {
+                        $this->transbordo = 0;
+                        return true;
+                    } else {
+                        $this->transbordo = 0;
+                        return false;
+                    }
+                }
+
+            }
+            //$var = "01-01";
+            //if(in_array($var,$diasferiados)){echo "TTTTTTTT";}
+
+            if ($diapago1 == "Domingo" || in_array($diapagoferiado, $diasferiados)) {
+                if ($horapago1 >= "06:00:00" && $horapago1 <= "22:00:00" && $diapago1 == $diapago2 && $diapago1prima == $diapago2prima) {
+                    echo "TODO EL DIA";
+                    $tolerancia += 90 * 60;
+                    $tolerancia = $tiempoayuda->saber_hora($tolerancia);
+                    echo "TOLERANCIA ", $tolerancia;
+                    echo "HORA PAGO 2 ", $horapago2;
+                    if ($horapago2 <= $tolerancia) {
+                        $this->transbordo = 0;
+                        return true;
+                    } else {
+                        $this->transbordo = 0;
+                        return false;
+                    }
+                }
+
+                if ($horapago1 > "22:00:00" || $horapago1 < "06:00:00") {
+                    $tolerancia += 90 * 60;
+                    $tolerancia = $tiempoayuda->saber_hora($tolerancia);
+                    if ($horapago2 <= $tolerancia) {
+                        $this->transbordo = 0;
+                        return true;
+                    } else {
+                        $this->transbordo = 0;
+                        return false;
+                    }
+                }
+            }
+        } else {
+            return false;
+        }
+    }
+
     public function recargar($monto, int $tiempo) {
         global $VALORES_CARGABLES;
 
-        if (!$VALORES_CARGABLES->contains((float) $monto)) return false;
-        $this->saldo += valorCargado((float) $monto);
+        if (!$VALORES_CARGABLES->contains((float)$monto)) return false;
+        $this->saldo += valorCargado((float)$monto);
         return true;
     }
 
@@ -183,7 +180,7 @@ class Tarjeta implements TarjetaInterface {
     public function generarPago(int $tiempo): Pago {
         $pago = $this->manejarPago($tiempo);
 
-        if(!$pago->FALLO) {
+        if (!$pago->FALLO) {
             $this->alFinalizarPago($tiempo);
         }
 
@@ -197,16 +194,16 @@ class Tarjeta implements TarjetaInterface {
     private function manejarPago(int $tiempo): Pago {
         global $MAX_PLUS;
 
-        if($this->getPrecio($tiempo)->NO_SE_PUEDE) {
+        if ($this->getPrecio($tiempo)->NO_SE_PUEDE) {
             return Pago::newFallado();
         }
 
-        if($this->getPrecio($tiempo)->PRECIO == 0) {
-            return new Pago(false, $this->getPrecio($tiempo),false);
+        if ($this->getPrecio($tiempo)->PRECIO == 0) {
+            return new Pago(false, $this->getPrecio($tiempo), false);
         }
 
-        if($this->obtenerSaldo() - $this->getPrecio($tiempo)->PRECIO < 0) {
-            if($this->boletosPlusUsados >= $MAX_PLUS) return Pago::newFallado();
+        if ($this->obtenerSaldo() - $this->getPrecio($tiempo)->PRECIO < 0) {
+            if ($this->boletosPlusUsados >= $MAX_PLUS) return Pago::newFallado();
 
             $this->boletosPlusUsados++;
             return new Pago(false, $this->getPrecio($tiempo), true);
@@ -214,15 +211,15 @@ class Tarjeta implements TarjetaInterface {
 
         $extra = [];
 
-        if($this->boletosPlusUsados > 0) {
-            $boletosPlusAPagar = ($this->boletosPlusUsados)*$this->getPrecio($tiempo)->PRECIO;
+        if ($this->boletosPlusUsados > 0) {
+            $boletosPlusAPagar = ($this->boletosPlusUsados) * $this->getPrecio($tiempo)->PRECIO;
 
-            if($this->saldo - $boletosPlusAPagar + $this->getPrecio($tiempo)->PRECIO < 0) return Pago::newFallado();
+            if ($this->saldo - $boletosPlusAPagar + $this->getPrecio($tiempo)->PRECIO < 0) return Pago::newFallado();
 
             $this->saldo -= $boletosPlusAPagar;
             $this->boletosPlusUsados = 0;
 
-            $extra[] = "Abona viajes plus $".($boletosPlusAPagar);
+            $extra[] = "Abona viajes plus $" . ($boletosPlusAPagar);
         }
 
         $this->saldo -= $this->getPrecio($tiempo)->PRECIO;
