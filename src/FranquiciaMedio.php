a00b61bdfa723b96d5c403d4f43298e46b93d2a8
Emmanuel M <emmanuelbendavid@gmail.com>
2018-09-25 23:38:22 -0300

    Implementado transbordo

--- src/FranquiciaMedio.php
+++ src/FranquiciaMedio.php
@@ -6,15 +6,20 @@ class FranquiciaMedio extends Tarjeta {
 
     private $pagosHoy = 0;
 
-    public function getPrecio(int $tiempo): Precio {
-        global $PRECIO_VIAJE;
+    public function getPrecio(int $tiempo, ColectivoInterface $colectivo): Precio {
         global $PRECIO_MEDIO_BOLETO;
+        global $PRECIO_RELATIVO_TRANSBORDO;
 
         if($this->pagosHoy >= 2 && TiempoAyudante::pertenecenAlMismoDia($this->getUltTiempo(), $tiempo)) {
-            return new Precio(false, $PRECIO_VIAJE, TipoDeBoleto::Normal);
+            return parent::getPrecio($tiempo, $colectivo);
         }
 
         $noSePuede = $tiempo - $this->getUltTiempo() < TiempoAyudante::CINCO_MINUTOS;
+
+        if($this->estransbordo($tiempo, $colectivo->linea(), $colectivo->numero())) {
+            return new Precio($noSePuede, $PRECIO_MEDIO_BOLETO*$PRECIO_RELATIVO_TRANSBORDO, TipoDeBoleto::Trans);
+        }
+
         return new Precio($noSePuede, $PRECIO_MEDIO_BOLETO, TipoDeBoleto::Medio);
     }
 
